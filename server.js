5985e66c4fa40fa0738b9d96658068cfb43c6819
JanLascala <lascala.jan@gmail.com>
2025-03-12 16:00:50 +0100

    Exe completed

--- server.js
+++ server.js
@@ -21,9 +21,46 @@ app.get('/', (req, res) => {
 
 //add img and blog i suppose
 
-app.delete('/api/v1/myFoodBlog', (req, res) => {
-    const bakery = [{
+app.get('/api/v1/bacheca', (req, res) => {
+    const bakery = [
+        {
+            id: 1,
+            title: "Ciambellone",
+            content: "Ring cake",
+            image: "/Img/ciambellone.jpeg",
+            tags: ["dessert", "Breakfast", "cake"]
+        },
+        {
+            id: 2,
+            title: "Cracker Barbabietola",
+            content: "Beetroot crackers",
+            image: "/Img/cracker_barbabietola.jpeg",
+            tags: ["snack", "natural", "beetroot"]
+        },
+        {
+            id: 3,
+            title: "Pane Fritto Dolce",
+            content: "Sweet fried bread",
+            image: "/Img/pane_fritto_dolce.jpeg",
+            tags: ["sweet", "fried", "fatty"]
+        },
+        {
+            id: 4,
+            title: "Pasta Barbabietola",
+            content: "Homemade beetroot pasta",
+            image: "/Img/pasta_barbabietola.jpeg",
+            tags: ["pasta", "lunch", "beetroot"]
+        },
+        {
+            id: 5,
+            title: "Torta Paesana",
+            content: "A rustic cake",
+            image: "/Img/torta_paesana.jpeg",
+            tags: ["cake", "rustic", "traditional"]
+        }
+    ];
 
-    }]
+
+    res.json(bakery)
 
 })
