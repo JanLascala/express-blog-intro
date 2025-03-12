defd9e1df96ca4d93073a7afb29ed763d1219cd5
JanLascala <lascala.jan@gmail.com>
2025-03-12 14:54:46 +0100

    Add progress init add content img etc

--- server.js
+++ server.js
@@ -1,4 +1,29 @@
 //js express basic
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3001;
+
+// assett img
+app.use(express.static('public'));
+
+// server listening port 3000
+app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`)
+})
+
+//home route
+app.get('/', (req, res) => {
+    console.log('home route requested')
+
+    res.send('Server del mio blog')
+
+})
+
+//add img and blog i suppose
+
+app.delete('/api/v1/myFoodBlog', (req, res) => {
+    const bakery = [{
+
+    }]
+
+})
